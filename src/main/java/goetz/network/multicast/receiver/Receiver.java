b2a0946f493b6d07759cd1216942e1ec9871cb8d
michael.goetz <michael.goetz@altamiracorp.com>
2016-07-13 15:00:49 -0400

    Added basic functionality
    Still requires configuration and better thread handling

--- src/main/java/goetz/network/multicast/receiver/Receiver.java
+++ src/main/java/goetz/network/multicast/receiver/Receiver.java
@@ -1,5 +1,36 @@
 package goetz.network.multicast.receiver;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class Receiver {
+	
+	MulticastSocket socket;
+	InetAddress group;
+	
+	public Receiver() throws IOException{
+		socket = new MulticastSocket(4222);
+		group = InetAddress.getByName("239.1.2.3");
+		socket.joinGroup(group);
+	}
+	
+	public void receive() throws IOException{
+		DatagramPacket packet;
+		
+		while(true){
+			byte[] buffer = new byte[256];
+			packet = new DatagramPacket(buffer, buffer.length);
+			socket.receive(packet);
+			
+			System.out.println("RECV: " + new String(packet.getData()));
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Receiver receiver = new Receiver();
+		receiver.receive();
+	}
 
 }
--- src/main/java/goetz/network/multicast/sender/Sender.java
+++ src/main/java/goetz/network/multicast/sender/Sender.java
@@ -1,5 +1,28 @@
 package goetz.network.multicast.sender;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Sender {
+
+	public void send(String message) throws IOException {
+		byte[] buffer = message.getBytes();
+
+		InetAddress group = InetAddress.getByName("239.1.2.3");
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, 4222);
+		
+		DatagramSocket socket = new DatagramSocket(5151);
+		socket.send(packet);
+		
+		System.out.println("Message sent");
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Sender sender = new Sender();
+		sender.send("testmessage");
+	}
 
 }
