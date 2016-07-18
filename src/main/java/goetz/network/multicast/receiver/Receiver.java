2402f7f1e7a334b5cddeb48fd38d57ee56f1c1fe
michael.goetz <michael.goetz@altamiracorp.com>
2016-07-18 16:48:59 -0400

    initial functional commit

--- src/main/java/goetz/network/multicast/receiver/Receiver.java
+++ src/main/java/goetz/network/multicast/receiver/Receiver.java
@@ -1,5 +1,29 @@
 package goetz.network.multicast.receiver;
 
-public class Receiver {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 
+public class Receiver {
+	public static void main(String[] args) {
+		MulticastSocket socket = null;
+		DatagramPacket inPacket = null;
+		byte[] inBuf = new byte[256];
+		try {
+			// Prepare to join multicast group
+			socket = new MulticastSocket(8888);
+			InetAddress address = InetAddress.getByName("224.2.2.3");
+			socket.joinGroup(address);
+
+			while (true) {
+				inPacket = new DatagramPacket(inBuf, inBuf.length);
+				socket.receive(inPacket);
+				String msg = new String(inBuf, 0, inPacket.getLength());
+				System.out.println("From " + inPacket.getAddress() + " Msg : " + msg);
+			}
+		} catch (IOException ioe) {
+			System.out.println(ioe);
+		}
+	}
 }
